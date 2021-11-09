85389b1ece0355aa44fa2cf47537e0a322a9d562
mikelangelo <mikelangelorz@gmail.com>
2021-11-09 12:20:59 +0100

    Primmer commit

---
+++ app/src/main/java/com/example/primeratarea/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.primeratarea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        androidx.appcompat.widget.Toolbar miToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(miToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
