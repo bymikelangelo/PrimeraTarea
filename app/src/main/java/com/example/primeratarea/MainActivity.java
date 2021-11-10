7c5c80bf570b784078e64c8741e143dc9898d876
mikelangelo <mikelangelorz@gmail.com>
2021-11-10 03:41:40 +0100

    Creada Main Activity (martes 10 nov)

--- app/src/main/java/com/example/primeratarea/MainActivity.java
+++ app/src/main/java/com/example/primeratarea/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.primeratarea;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textDescrAct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        androidx.appcompat.widget.Toolbar miToolbar = findViewById(R.id.my_toolbar);
+        androidx.appcompat.widget.Toolbar miToolbar = findViewById(R.id.miToobar);
         setSupportActionBar(miToolbar);
+
+        textDescrAct = findViewById(R.id.textDescrAct);
     }
 
     @Override
@@ -23,5 +29,37 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void verDescripcion (View vista) {
+        switch (vista.getId()) {
+            case R.id.botonDNI:
+                textDescrAct.setText(R.string.descripcion_dni);
+                break;
+            case R.id.botonConv:
+                textDescrAct.setText(R.string.descripcion_conversor);
+                break;
+            case R.id.botonImages:
+                textDescrAct.setText(R.string.descripcion_imagenes);
+                break;
+        }
+    }
+
+    public void irAActivity (View vista) {
+        Intent intencion;
+        switch (vista.getId()) {
+            case R.id.menuDNI:
+                intencion = new Intent(this, DNIActivity.class);
+                startActivity(intencion);
+                break;
+            case R.id.menuConversor:
+                intencion = new Intent(this, ConversorActivity.class);
+                startActivity(intencion);
+                break;
+            case R.id.menuImages:
+                intencion = new Intent(this, ImagesActivity.class);
+                startActivity(intencion);
+                break;
+        }
     }
 }
